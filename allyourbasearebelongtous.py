326caaafe66068442261f8819d3fc64e4fb418f4
Maria Rose Turing Snowden <mariarosesnowden@protonmail.com>
2022-04-20 15:58:27 +0200

    Add files via upload
    We take what we want without ANY PERMISSION.
    
    Kiss our asses!

---
+++ allyourbasearebelongtous.py
@@ -0,0 +1,13 @@
+Web Application Hacking Notes
+
+What is hacking? To me, the act of hacking is the tinkering, studying, analyzing, learning,
+exploring and experimenting. Not just computers, but anything. One of the great outcomes
+of this activity is discovering ways to make the object of your attention bend to your will for
+your benefi t, under your control.
+
+How to get the tree of the website@
+https://www.youtube.com/watch?v=LTjo4u4soHw
+https://www.youtube.com/watch?v=yrfsfSktehs
+
+https://wfuzz.readthedocs.io/
+
